309693f33fb38485926dd573d5a67bff68b28996
Rohan2198 <144374331+Rohan2198@users.noreply.github.com>
2023-12-08 05:43:14 -0500

    Add files via upload

---
+++ motor_sequence.c
@@ -0,0 +1,58 @@
+/*
+ * motor_sequence.c
+ *
+ * Created: 12/2/2023 11:09:08 PM
+ *  Author: rogun
+ */ 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include "motor_sequence.h"
+
+
+void clockwise_motor1_seq()
+{
+	PORTD |= (1<<PORTD2);
+	PORTD |= (1<<PORTD5);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD5);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD3);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD2);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD4);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD3);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD5);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD4);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD5);
+	_delay_ms(2);
+}
+
+void counter_clockwise_motor1_seq()
+{
+	PORTD |= (1<<PORTD5);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD4);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD5);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD3);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD4);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD2);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD3);
+	_delay_ms(2);
+	PORTD |= (1<<PORTD5);
+	_delay_ms(2);
+	PORTD &= ~(1<<PORTD2);
+	PORTD &= ~(1<<PORTD5);
+	_delay_ms(2);
+}
+
---
+++ motor_sequence.h
@@ -0,0 +1,19 @@
+/*
+ * motor_sequence.h
+ *
+ * Created: 12/2/2023 11:10:22 PM
+ *  Author: rogun
+ */ 
+
+
+#ifndef MOTOR_SEQUENCE_H_
+#define MOTOR_SEQUENCE_H_
+void clockwise_motor1_seq();
+void counter_clockwise_motor1_seq();
+void clockwise_motor2_seq();
+void counter_clockwise_motor2_seq();
+
+
+
+
+#endif /* MOTOR_SEQUENCE_H_ */
